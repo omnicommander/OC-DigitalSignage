2765eb28b3e97bff85541c87dea4b0ff6b1a6df3
Noah Beach <noah@omnicommander.com>
2022-05-13 14:02:28 -0500

    rewrite of install script

--- config.sh
+++ config.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-JSONPATH="/home/pi/OC-DigitalSignage/json"          	    # path for json file from server.
-VIDEOPATH="/home/pi/OC-DigitalSignage/videos"                  # path for fetch to store video mp4 files
-fetchLog="/home/pi/OC-DigitalSignage/fetch.log"               # path for logging
-TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`               # timestamp formatting for logger
-# URL="http://34.193.150.151/pi_callHome.php?id="     # url to server for requests
-URL="https://3.17.246.205/pi_callHome.php?id="
-AUTHOR="Eric Bradshaw bradshaw@omnicommander.com"   # author
-PI_UID="OC-ConferenceRoom"   	                                    # ID for this PI machine
-VERSION=1.7.1                                        # revision number
+JSONPATH="/home/pi/OC-DigitalSignage/json"
+VIDEOPATH="/home/pi/OC-DigitalSignage/videos"
+fetchLog="/home/pi/OC-DigitalSignage/fetch.log"
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+URL="https://omnicommando.com/pi_callHome.php?id="
+AUTHOR="Eric Bradshaw bradshaw@omnicommander.com"
+VERSION="v1.7.1"
+PI_UID="OC-ConferenceRoom"
